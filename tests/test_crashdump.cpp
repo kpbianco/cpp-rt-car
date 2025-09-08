7957e38b52e50c9b85941c91534826327333d46c
Kian Bianco <52121367+kpbianco@users.noreply.github.com>
2025-09-08 13:58:49 -0700

    Skip crashdump test on Windows

--- tests/test_crashdump.cpp
+++ tests/test_crashdump.cpp
@@ -5,6 +5,9 @@
 #include <chrono>
 
 TEST(CrashDump, WritesBacktrace) {
+#ifdef _WIN32
+    GTEST_SKIP() << "Crashdump not implemented on Windows";
+#else
     const char* path = "crashdump_test.txt";
     rt::write_crashdump_async(path);
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -13,5 +16,6 @@ TEST(CrashDump, WritesBacktrace) {
     std::getline(in, line);
     EXPECT_FALSE(line.empty());
     std::remove(path);
+#endif
 }
 
